858bedef02fdaf30414b091c4d51516721519034
MilesBradt <smbradtmichael@gmail.com>
2019-12-18 02:26:51 -0800

    1.0

---
+++ SinglePlayerBedRegen/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace SinglePlayerBedRegen {
+    public class ModEntry : Mod {
+        public override void Entry(IModHelper helper) {
+            helper.Events.GameLoop.UpdateTicked += this.UpdateTicked;
+        }
+
+        private void UpdateTicked(object sender, UpdateTickedEventArgs e) {
+            if (!Context.IsPlayerFree || Game1.paused || Game1.IsMultiplayer)
+                return;
+
+            //Health
+            if (e.IsMultipleOf(30)) {
+                if (Game1.player.isInBed)
+                    if (Game1.player.health < Game1.player.maxHealth)
+                        Game1.player.health = Math.Min(Game1.player.maxHealth, Game1.player.health + 1);
+            }
+
+            //Stamina
+            if (e.IsMultipleOf(30)) {
+                if (Game1.player.isInBed)
+                    if (Game1.player.Stamina < Game1.player.MaxStamina)
+                        Game1.player.Stamina = Math.Min(Game1.player.MaxStamina, Game1.player.Stamina + 1);
+            }
+        }
+    }
+}
